a6781563e197ee23b81ae0d56d5008e1672c3063
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-07 16:44:53 -0300

    Third lesson: If & Comparisson operator

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 29;
-prompt('Escolha um numero entre 1 e 30');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um numero entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('isso ai! Você descobriu o numero secreto (5)');
+} 
