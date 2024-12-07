63394d9cd8ab22acf58092d853d00af78128ce4c
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-07 17:13:43 -0300

    First challenge: Variables, alerts, comparissons and prompts

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,24 @@
+//FIRST CHALLENGE
+
+//Start-------------------------------------
+alert('Bos vindas ao nosso site!');
+
+let nome = 'Lua';
+let idade = 25;
+let numeroDeVendar = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos campos');
+
+let mensagemDeErro = 'Erro! Preencha todos os campos campos';
+
+alert(mensagemDeErro);
+
+nome = prompt('Digite seu nome')
+idade = prompt('Digite sua idade')
+
+if(idade >= 18) {
+    alert('Pode tirar a habilitação!')
+}
+//End---------------------------------------
+
