7f08e8697417425cf6f06d043a358cfe6cdca2fb
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-08 12:08:36 -0300

    Ninth lesson: Attempts counter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao n.s
 while (chute != numeroSecreto) {
@@ -9,12 +10,13 @@ while (chute != numeroSecreto) {
     chute = prompt('Escolha um numero entre 1 e 10');
 
     if (chute == numeroSecreto) {
-        alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto}`);
+        alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         } else {
-            alert(`O número secreto é menos que ${chute}`)
+            alert(`O número secreto é menos que ${chute}`);
         }
     }
+    tentativas++;
 }
