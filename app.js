c502bc441c4227f195fdb0426511c4852c727060
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-07 22:42:33 -0300

    Fourth Lesson: Comments and Else

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um numero entre 1 e 10');
 
+// se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('isso ai! Você descobriu o numero secreto (5)');
-} 
+    alert('isso ai! Você descobriu o numero secreto (5)');
+} else {
+    alert('Você errou :(')
+}
