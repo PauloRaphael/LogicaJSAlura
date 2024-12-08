9d1b5bc9a2d8b34e44fdae4cdf53d6ebadc9540a
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-07 22:46:24 -0300

    Fifth lesson: Concatenation

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um numero entre 1 e 10');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('isso ai! Você descobriu o numero secreto (5)');
+    alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto}`);
 } else {
     alert('Você errou :(')
 }
