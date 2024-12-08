dd959ba9dc7aa33abc9a30ed02042eb1a89b42ef
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-08 12:23:19 -0300

    Tenth lesson: Break

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ let tentativas = 1;
 
 //enquanto chute não for igual ao n.s
 while (chute != numeroSecreto) {
-    // se chute for igual ao número secreto
     chute = prompt('Escolha um numero entre 1 e 10');
-
+    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`);
@@ -19,4 +18,10 @@ while (chute != numeroSecreto) {
         }
     }
     tentativas++;
+}
+
+if(tentativas > 1) {
+    alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+} else {
+    alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
 }
