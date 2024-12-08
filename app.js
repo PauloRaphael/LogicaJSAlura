238ed1cb852fb1553aab4ce9c8eebcb4a6ac71d9
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-08 12:05:22 -0300

    Eighth lesson: While

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 9;
 console.log(numeroSecreto)
-let chute = prompt('Escolha um numero entre 1 e 10');
+let chute;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto}`);
-} else {
-    if(chute > numeroSecreto) {
-        alert(`O número secreto é maior que ${chute}`)
+//enquanto chute não for igual ao n.s
+while (chute != numeroSecreto) {
+    // se chute for igual ao número secreto
+    chute = prompt('Escolha um numero entre 1 e 10');
+
+    if (chute == numeroSecreto) {
+        alert(`isso ai! Você descobriu o numero secreto ${numeroSecreto}`);
     } else {
-        alert(`O número secreto é menos que ${chute}`)
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}`)
+        } else {
+            alert(`O número secreto é menos que ${chute}`)
+        }
     }
 }
