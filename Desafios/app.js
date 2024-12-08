0c5d7f29f6a616a4d439c6ac0d0f6b3f4db85598
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-07 23:03:21 -0300

    Second challenge: Conditionals and concatenation

--- Desafios/app.js
+++ Desafios/app.js
@@ -1,24 +1,65 @@
 //FIRST CHALLENGE
 
 //Start-------------------------------------
-alert('Bos vindas ao nosso site!');
+// alert('Bos vindas ao nosso site!');
 
-let nome = 'Lua';
-let idade = 25;
-let numeroDeVendar = 50;
-let saldoDisponivel = 1000;
+// let nome = 'Lua';
+// let idade = 25;
+// let numeroDeVendar = 50;
+// let saldoDisponivel = 1000;
 
-alert('Erro! Preencha todos os campos campos');
+// alert('Erro! Preencha todos os campos campos');
 
-let mensagemDeErro = 'Erro! Preencha todos os campos campos';
+// let mensagemDeErro = 'Erro! Preencha todos os campos campos';
 
-alert(mensagemDeErro);
+// alert(mensagemDeErro);
 
-nome = prompt('Digite seu nome')
-idade = prompt('Digite sua idade')
+// nome = prompt('Digite seu nome')
+// idade = prompt('Digite sua idade')
 
-if(idade >= 18) {
-    alert('Pode tirar a habilitação!')
+// if(idade >= 18) {
+//     alert('Pode tirar a habilitação!')
+// }
+//End---------------------------------------
+
+//SECOND CHALLENGE
+
+//Start-------------------------------------
+let diaDaSemana = prompt('Qual o dia da semana hoje?');
+
+if(diaDaSemana.toLowerCase() == 'sábado' || diaDaSemana.toLowerCase() == 'domingo') {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
 }
+
+//__________
+
+let numero = prompt('Digite um número');
+
+if(numero < 0) {
+    alert('O número é negativo -');
+} else {
+    alert('O número é positivo +');
+}
+
+//__________
+let pontuação = 40;
+
+if(pontuação >= 100) {
+    alert('Parabéns, você venceu!')
+} else {
+    alert('Tente novamente para ganhar.')
+}
+
+//_________
+let saldo = 1520.50;
+
+alert(`Seu saldo é ${saldo}`);
+
+//_________
+let nome = prompt('Digite seu nome');
+
+alert(`Seja bem vindo ${nome}!`)
 //End---------------------------------------
 
